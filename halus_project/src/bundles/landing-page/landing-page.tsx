bff4b82ccfc4dedef44fe84fa7cf95edc4fb7c6d
Maxik <mpoplavskiy38380@gmail.com>
2024-03-12 09:27:18 +0200

    refactor landing page

--- halus_project/src/bundles/landing-page/landing-page.tsx
+++ halus_project/src/bundles/landing-page/landing-page.tsx
@@ -1,7 +1,4 @@
 import { Header } from "../header/header";
-import IntroImage from '../../assets/images/intro.png';
-import LineChart from '../../assets/images/image 13.png';
-import DonutChart from '../../assets/images/image 14.png';
 import TikTok from '../../assets/images/tiktok.png';
 import Instagram from '../../assets/images/instagram.png';
 import XLogo from '../../assets/images/X.png';
@@ -9,6 +6,7 @@ import Sofia from '../../assets/images/407224620_893420559116604_893671142775734
 import styles from './styles.module.scss';
 import { SocialMediaCard } from "./components/social-media-card/social-media-card";
 import { Footer } from "../footer/footer";
+import { CgProfile } from "react-icons/cg";
 
 function LandingPage() {
     return <>
@@ -18,45 +16,105 @@ function LandingPage() {
         <section className={styles.landing_page__intro_container}>
             <div className={styles.intro__text_container}>
                 <h1 className={styles.intro__title}>
-                    Intro
+                    PromoPulse
                 </h1>
 
-                <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                <h3 className={styles.intro__subtitle}>
+                Your pulse of success in the digital world!
+                </h3>
+
+                <p className={styles.intro__text}>
+                    Створюйте контент, плануйте, захоплюйте! 
+                    <br />
+                    Вдосконалюйте ваші стратегії у соціальних мережах!
                 </p>
-            </div>
-
-            <div className={styles.intro__image_container}>
-                <img src={IntroImage} />
+                <div className={styles.intro__button_container}>
+                    <button className={styles.button_red}>Start now</button>
+                    <button className={styles.button_white}>Contact sales</button>
+                </div>
             </div>
         </section>
 
-        <section className={styles.landing_page__statistics_container}>
-            <h1 className={styles.landing_page__statistics_title}>
-                AI statistic
-            </h1>
+        <section className={styles.landing_page__templates}>
+            <div className={styles.landing_page__cards_container}>
+                <div className={styles.landing_page__template_card}>
+                    <div className={styles.template_card__image_purple}>
+                    </div>
+                    <div className={styles.template_card__content}>
+                        <div className={styles.template_card__content_upper_body}>
+                            <div className={styles.template_card__avatar}>
 
-            <img src={LineChart}></img>
-            
-            <img src={DonutChart}></img>
-        </section>
+                            </div>
+                            <div className={styles.template_title_container}>
+                                <h2 className={styles.template_title}>
+                                    TikTok
+                                </h2>
+                                <p className={styles.template_subtitle}>
+                                    Secondary text
+                                </p>
+                            </div>
+                        </div>
 
-        <section className={styles.landing_page__recommendation_container}>
-            <h1>
-                AI recommendation
-            </h1>
+                        <p>
+                            Greyhound divisively hello coldly wonderfully marginally far upon excluding.
+                        </p>
 
-            <ol>
-                <li>
-                    Experiment with brighter compositions to add vibrancy and allure to your images. Consider adjusting exposure settings or exploring different lighting conditions to capture the essence of your subjects in a more radiant and captivating manner.
-                </li>
-                <li>
-                    Enhance your audience engagement by establishing a consistent posting schedule. Aim to share your photography work on a daily basis to maintain a regular presence and keep your followers intrigued and connected to your evolving portfolio.
-                </li>
-                <li>
-                    Explore the potential of posting during the evening hours to leverage the enchanting ambiance of twilight and dusk. Experiment with capturing the interplay of natural and artificial light to create captivating and atmospheric photographs that resonate with your audience's emotions and experiences.
-                </li>
-            </ol>
+                        <button className={styles.template_card__button_red}>EDIT&nbsp;NOW</button>
+                    </div>
+                </div>
+
+                <div className={styles.landing_page__template_card}>
+                    <div className={styles.template_card__image_blue}>
+                    </div>
+                    <div className={styles.template_card__content}>
+                        <div className={styles.template_card__content_upper_body}>
+                            <div className={styles.template_card__avatar}>
+
+                            </div>
+                            <div className={styles.template_title_container}>
+                                <h2 className={styles.template_title}>
+                                    Instagram
+                                </h2>
+                                <p className={styles.template_subtitle}>
+                                    Secondary text
+                                </p>
+                            </div>
+                        </div>
+
+                        <p>
+                            Greyhound divisively hello coldly wonderfully marginally far upon excluding.
+                        </p>
+
+                        <button className={styles.template_card__button_red}>EDIT&nbsp;NOW</button>
+                    </div>
+                </div>
+
+                <div className={styles.template_column__container}>
+                    <button className={styles.template_column__button_add}>+</button>
+
+                    <div className={styles.template_column__labels_container}>
+                    <div className={styles.template_column__label_container}>
+                            <CgProfile className={styles.template_column__label_icon} />
+                            <p className={styles.template_column__label_text}>Label</p>
+                        </div>
+
+                        <div className={styles.template_column__label_container}>
+                            <CgProfile className={styles.template_column__label_icon} />
+                            <p className={styles.template_column__label_text}>Label</p>
+                        </div>
+
+                        <div className={styles.template_column__label_container}>
+                            <CgProfile className={styles.template_column__label_icon} />
+                            <p className={styles.template_column__label_text}>Label</p>
+                        </div>
+
+                        <div className={styles.template_column__label_container}>
+                            <CgProfile className={styles.template_column__label_icon} />
+                            <p className={styles.template_column__label_text}>Label</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
 
         <section className={styles.landing_page__social_media_container}>
