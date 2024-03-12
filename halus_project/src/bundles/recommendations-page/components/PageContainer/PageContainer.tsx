0a0926eeac88640779e06fde7e1e48c5cd459d5d
kovalsofiia1 <kovalsofiia1@gmail.com>
2024-03-12 18:03:03 +0200

    cards

--- halus_project/src/bundles/recommendations-page/components/PageContainer/PageContainer.tsx
+++ halus_project/src/bundles/recommendations-page/components/PageContainer/PageContainer.tsx
@@ -5,8 +5,8 @@ import { useState } from 'react'
 
 const recs = [
   'Experiment with brighter compositions to add vibrancy and allure to your images. Consider adjusting exposure settings or exploring different lighting conditions to capture the essence of your subjects in a more radiant and captivating manner.',
-              'Enhance your audience engagement by establishing a consistent posting schedule. Aim to share your photography work on a daily basis to maintain a regular presence and keep your followers intrigued and connected to your evolving portfolio.',
-             'Explore the potential of posting during the evening hours to leverage the enchanting ambiance of twilight and dusk. Experiment with capturing the interplay of natural and artificial light to create captivating and atmospheric photographs that resonate with your audience\'s emotions and experiences.'
+  'Enhance your audience engagement by establishing a consistent posting schedule. Aim to share your photography work on a daily basis to maintain a regular presence and keep your followers intrigued and connected to your evolving portfolio.',
+  'Explore the potential of posting during the evening hours to leverage the enchanting ambiance of twilight and dusk. Experiment with capturing the interplay of natural and artificial light to create captivating and atmospheric photographs that resonate with your audience\'s emotions and experiences.'
 ]
 
 export function PageContainer() {
